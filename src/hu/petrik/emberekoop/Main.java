dd76068c9321594b191a1461d104e7bd0bd483a9
Budaházi Bence <budahazi.bence@petrik.hu>
2022-09-20 13:57:05 +0200

    Ember osztály - adattagok - paraméteres konstruktor - toString
    Main osztály
    - Ember felvétele és kiiratása

--- src/hu/petrik/emberekoop/Main.java
+++ src/hu/petrik/emberekoop/Main.java
@@ -2,6 +2,7 @@ package hu.petrik.emberekoop;
 
 public class Main {
     public static void main(String[] args) {
-        
+        Ember human = new Ember("Gipsz Jakab", "2001-1-1","Budapest");
+        System.out.println(human);
     }
 }
